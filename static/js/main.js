6d24ecdf5b3fa976a411b5ca325b18fc8c080082
roy  barrera <53977154+roybarrera@users.noreply.github.com>
2020-07-28 21:22:38 -0400

    cambios

--- static/js/main.js
+++ static/js/main.js
@@ -45,7 +45,7 @@ $(document).ready(function () {
                 // Get and display the result
                 $('.loader').hide();
                 $('#result').fadeIn(600);
-                $('#result').text(' El modelo predijo que la imagen pertenece a la categoría:  ' + data);
+                $('#result').text(' La imagen pertenece a la categoría:  ' + data);
                 console.log('Success!');
             },
         });
